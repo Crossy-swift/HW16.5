cbcb0e5ff7094b9b4c90328669919b3a72710ab0
Crossy-swift <crossworm2011@gmail.com>
2022-07-10 16:55:31 +0300

    Initial Commit

---
+++ HW16.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW16.5
+//
+//  Created by Артем Репин on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
